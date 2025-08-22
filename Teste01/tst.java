a24d73f1b1ffd2ea1356bfdca107c71adfb8b7d7
João Guilherme D. Morinari <joaogmktdg@gmail.com>
2025-08-22 09:28:25 -0300

    Add files via upload

---
+++ Teste01/tst.java
@@ -0,0 +1,26 @@
+
+/**
+ * Escreva uma descrição da classe tst aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class tst
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int x;
+
+    /**
+     * Construtor para objetos da classe tst
+     */
+    public class Teste01
+{
+    public static void main (String args[])
+    {
+            
+                    System.out.print("Hello World");
+        
+    
+
+    
+
